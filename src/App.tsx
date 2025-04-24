494e196177b26bf5fbe84c1fccb140f3cd83ba9a
hossainrezaeian <hossainrezaeian@gmail.com>
2025-04-24 21:45:51 +0330

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+import { Router } from './Router';
+import { theme } from './theme';
+import { Provider } from 'react-redux';
+import { store } from './app/srtore';
+
+export default function App() {
+  return (
+    <MantineProvider theme={theme}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+
+    </MantineProvider>
+  );
+}
